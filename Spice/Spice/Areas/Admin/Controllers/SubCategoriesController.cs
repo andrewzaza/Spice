952a5f517f00a0609539351ef325e43917863e94
andrewzaza <andrew_zaza2012@yahoo.com>
2020-12-12 16:56:45 +0200

    end lec3

--- Spice/Spice/Areas/Admin/Controllers/SubCategoriesController.cs
+++ Spice/Spice/Areas/Admin/Controllers/SubCategoriesController.cs
@@ -82,5 +82,107 @@ namespace Spice.Areas.Admin.Controllers
 
             return Json(new SelectList(subCategories, "ID", "Name"));
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await db.SubCategories.FindAsync(id);
+          
+            if (subCategory == null)
+            {
+                return NotFound();
+
+            }
+
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoriesList = await db.Categories.ToListAsync(),
+                SubCategory = subCategory
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesExistSubCatgory = await db.SubCategories.Include(m => m.Category)
+                    .Where(m => m.Category.Id == model.SubCategory.CategoryId
+                    && m.Name == model.SubCategory.Name 
+                    && m.ID!= model.SubCategory.ID).ToListAsync();
+
+                if (doesExistSubCatgory.Count() > 0)
+                {
+                    StatusMessage = "Error: This is Sub Category Exists under "
+                        + doesExistSubCatgory.FirstOrDefault()
+                        .Category.Name + " Category";
+
+                }
+                else
+                {
+                    db.SubCategories.Update(model.SubCategory);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            SubCategoryAndCategoryViewModel modelv = new SubCategoryAndCategoryViewModel()
+            {
+                CategoriesList = await db.Categories.ToListAsync(),
+                SubCategory = model.SubCategory,
+                StatusMessage = StatusMessage
+
+            };
+            return View(modelv);
+        }
+        [HttpGet]
+        public  IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory =  db.SubCategories.Include(m=>m.Category).Where(m=>m.ID ==id).SingleOrDefault();
+
+            if (subCategory == null)
+            {
+                return NotFound();
+
+            }
+            return View(subCategory);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task <IActionResult> Delete(SubCategory subCategory)
+        {
+            db.SubCategories.Remove(subCategory);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Detalis(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = db.SubCategories.Include(m => m.Category).Where(m => m.ID == id).SingleOrDefault();
+
+            if (subCategory == null)
+            {
+                return NotFound();
+
+            }
+            return View(subCategory);
+        }
     }
 }
